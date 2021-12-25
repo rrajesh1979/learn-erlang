9fe735d74476689a75c48d217a5109b84039622f
Rajesh Rajagopalan <rrajesh1979@gmail.com>
2021-12-24 22:37:46 -0500

    Initial commit

---
+++ formatter.erl
@@ -0,0 +1,16 @@
+-module(formatter).
+-export([month_name/1]).
+
+month_name(1) -> "January";
+month_name(2) -> "February";
+month_name(3) -> "March";
+month_name(4) -> "April";
+month_name(5) -> "May";
+month_name(6) -> "June";
+month_name(7) -> "July";
+month_name(8) -> "August";
+month_name(9) -> "September";
+month_name(10) -> "October";
+month_name(11) -> "November";
+month_name(12) -> "December".
+
