d810ce7bde6419611433efea5a97c1f43a1755d1
Rajesh Rajagopalan <rrajesh1979@gmail.com>
2021-12-25 09:51:13 -0500

    2nd udpate

--- bank/mybank.erl
+++ bank/mybank.erl
@@ -1,11 +1,14 @@
 -module(mybank).
 
--export([start/0]).
+-export([start/0, stop/1]).
 -export([main_loop/0]).
 
 start() ->
     io:format("~n~nWelcome to my bank~n~n"),
     spawn(?MODULE, main_loop, []).
+
+stop(Pid) ->
+    Pid ! terminate.
 
 main_loop() ->
     receive
